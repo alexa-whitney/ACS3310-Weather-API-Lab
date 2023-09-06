1d80754d8ceff807ec23886e18751b8d9633f966
alexa-whitney <alexawhitney@gmail.com>
2023-09-06 12:29:59 -0600

    Challenge 4 Completed

--- weather.js
+++ weather.js
@@ -1,16 +1,12 @@
 // Functions 
-function getWeather(apikey, zip) {
+async function getWeather(apikey, zip) {
 	const units = 'imperial'
 	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
 
-	// Return the Promise directly
-	return fetch(path)
-		.then(res => {
-			if (!res.ok) {
-				throw new Error(res.statusText);
-			}
-			return res.json();
-		});
+	const res = await fetch(path)
+	const json = await res.json()
+
+	return json;
 }
 
 module.exports = {
