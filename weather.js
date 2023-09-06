f077d8fe9d09b083613784714f85d6433eabb189
alexa-whitney <alexawhitney@gmail.com>
2023-09-06 13:46:50 -0600

    Challenge 7 Completed

--- weather.js
+++ weather.js
@@ -1,24 +1,38 @@
-// Functions 
-async function getWeather(apikey, zip, unit = 'imperial') {
-	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`
+const BASE_URL = 'https://api.openweathermap.org/data/2.5/weather?';
 
-	const res = await fetch(path)
-	const json = await res.json()
+async function fetchWeather(url) {
+    const res = await fetch(url);
+    const json = await res.json();
 
-	// Organize the data
-	const organizedData = {
-		temperature: json.main.temp,
-		description: json.weather[0].description,
-		humidity: json.main.humidity,
-		temp_max: json.main.temp_max,
-		temp_min: json.main.temp_min,
-		city: json.name,
-		icon: json.weather[0].icon
-	};
+    return {
+        temperature: json.main.temp,
+        description: json.weather[0].description,
+        humidity: json.main.humidity,
+        temp_max: json.main.temp_max,
+        temp_min: json.main.temp_min,
+        city: json.name,
+        icon: json.weather[0].icon
+    };
+}
 
-	return organizedData;
+async function getWeatherByZip(apiKey, zip, unit = 'imperial') {
+    const url = `${BASE_URL}zip=${zip}&appid=${apiKey}&units=${unit}`;
+    return await fetchWeather(url);
+}
+
+async function getWeatherByCity(apiKey, city, unit = 'imperial') {
+    const url = `${BASE_URL}q=${city}&appid=${apiKey}&units=${unit}`;
+    return await fetchWeather(url);
+}
+
+async function getWeatherByGeo(apiKey, coords, unit = 'imperial') {
+    const { lat, lon } = coords;
+    const url = `${BASE_URL}lat=${lat}&lon=${lon}&appid=${apiKey}&units=${unit}`;
+    return await fetchWeather(url);
 }
 
 module.exports = {
-	getWeather
+    getWeatherByZip,
+    getWeatherByCity,
+    getWeatherByGeo
 };
