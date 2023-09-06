44cefcb24874b90cd85e1d2e29b9b02b3fc585ae
alexa-whitney <alexawhitney@gmail.com>
2023-09-06 12:56:02 -0600

    Challenge 5 Completed

--- weather.js
+++ weather.js
@@ -6,7 +6,18 @@ async function getWeather(apikey, zip) {
 	const res = await fetch(path)
 	const json = await res.json()
 
-	return json;
+	// Organize the data
+	const organizedData = {
+		temperature: json.main.temp,
+		description: json.weather[0].description,
+		humidity: json.main.humidity,
+		temp_max: json.main.temp_max,
+		temp_min: json.main.temp_min,
+		city: json.name,
+		icon: json.weather[0].icon
+	};
+
+	return organizedData;
 }
 
 module.exports = {
