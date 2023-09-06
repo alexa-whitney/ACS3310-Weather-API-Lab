5f56b62fd0e594c1cfc3dcfd307f013cc3e3464a
alexa-whitney <alexawhitney@gmail.com>
2023-09-06 13:31:56 -0600

    Challenge 6 Completed

--- weather.js
+++ weather.js
@@ -1,7 +1,6 @@
 // Functions 
-async function getWeather(apikey, zip) {
-	const units = 'imperial'
-	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${units}`
+async function getWeather(apikey, zip, unit = 'imperial') {
+	const path = `https://api.openweathermap.org/data/2.5/weather?zip=${zip}&appid=${apiKey}&units=${unit}`
 
 	const res = await fetch(path)
 	const json = await res.json()
